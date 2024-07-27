6728d5edd9cccc79532fee56fe4cc60795b5f178
drewbeno1 <benodrew12@gmail.com>
2024-07-26 20:40:53 -0400

    fix title

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <script setup>
-  import Button from './components/buttons/Button.vue'
 </script>
 
 <template>
-  <div class="flex flex-col items-center mt-20">
-    <Button/>
-  </div>
+  <router-view></router-view>
 </template>
 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script>
+  import Button from '../components/buttons/Button.vue';
+  import hey from './hey.vue';
+</script>
+
+<template>
+  <div class="flex flex-col items-center mt-20">
+    <hey/>
+    <Button/>
+  </div>
+</template>
