5fa728e9c841370c497152b1641a750d69eeba78
breinke27 <brian.reinke@richmond.edu>
2024-08-13 11:15:50 -0400

    fixing of typing for headshot.png and matching HV

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@ const route = useRoute();
     <banner />
     <div class="mt-16 text-center border-black border-4 rounded-lg p-8">
       <div class="flex flex-col md:flex-row items-center justify-center">
-        <img src="../assets/headshot.PNG" alt="Brian Reinke" class="w-64 h-auto shadow-lg mb-6 md:mb-0 md:mr-8 rounded-md" />
+        <img src="../assets/headshot.png" alt="Brian Reinke" class="w-64 h-auto shadow-lg mb-6 md:mb-0 md:mr-8 rounded-md" />
         <p class="text-lg leading-relaxed max-w-lg">
           Hello  and welcome to my portfolio! Here you will find a collection of projects I have worked on and my resume. I'm passionate about data engineering, data science, and web development. Thank you for taking the time to explore!
         </p>
