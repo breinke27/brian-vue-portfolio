f4ef8939a0a9064929a301137379dc76a83db66c
breinke27 <brian.reinke@richmond.edu>
2024-08-13 11:12:10 -0400

    fixing of type casing for static picture files

--- src/views/SecondView.vue
+++ src/views/SecondView.vue
@@ -11,15 +11,15 @@ import banner from './Banner.vue';
       <div class="flex flex-col space-y-8">
         <div class="flex flex-row justify-center space-x-4">
           <div class="flex flex-col items-center border-black border-4 p-4 rounded-lg">
-            <img src="../assets/smlogo.PNG" alt="SeedMetrics" class="w-72 h-auto object-cover mb-4 rounded-lg">
+            <img src="../assets/smlogo.png" alt="SeedMetrics" class="w-72 h-auto object-cover mb-4 rounded-lg">
             <Button title="SeedMetrics" link="/sm_project" class="flex justify-center" />
           </div>
           <div class="flex flex-col items-center border-black border-4 p-4 rounded-lg">
-            <img src="../assets/vaulted-logo.PNG" alt="Vaulted" class="w-72 h-auto object-cover mb-4 rounded-lg">
+            <img src="../assets/vaulted_logo.png" alt="Vaulted" class="w-72 h-auto object-cover mb-4 rounded-lg">
             <Button title="Vaulted Baseball" link="/vault_project" class="flex justify-center" />
           </div>
           <div class="flex flex-col items-center border-black border-4 p-4 rounded-lg">
-            <img src="../assets/db-logo.PNG" alt="dataBase" class="w-72 h-auto object-cover mb-4 rounded-lg">
+            <img src="../assets/db_logo.png" alt="dataBase" class="w-72 h-auto object-cover mb-4 rounded-lg">
             <Button title="dataBase Sabermetrics" link="/db_project" class="flex justify-center" />
           </div>
         </div>
