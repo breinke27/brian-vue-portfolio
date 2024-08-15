c06dc897cb8e57fac8fe523f657c11ee9675fd82
breinke27 <brian.reinke@richmond.edu>
2024-08-15 15:21:35 -0400

    addition of code links to github

--- src/views/Banner.vue
+++ src/views/Banner.vue
@@ -1,27 +1,4 @@
- <!-- <script setup>
- import Button from '../components/buttons/Button.vue';
- import { useRoute } from 'vue-router';
- 
- const route = useRoute();
- </script>
- 
- <template>
-   <div class="bg-gray-800 text-white py-4 w-full fixed top-0 left-0 flex justify-between items-center">
-     <span class="font-bold text-xl ml-4 border border-white rounded-lg px-2 py-1">
-       Brian Reinke's Portfolio
-     </span>
-     <div class="flex items-center">
-       <template v-if="route.path !== '/'">
-         <Button title="Home" link="/" />
-       </template>
-       <template v-if="route.path !== '/page2'">  
-         <Button title="Read More About My Projects" link="/page2" class="font-bold mr-4 rounded-lg"/>
-       </template>
-       <Button title="LinkedIn" link="https://www.linkedin.com/in/brianreinke21" target="_blank" class="font-bold mr-4 rounded-lg" />
-     </div>
-   </div>
- </template> -->
- <script setup>
+<script setup>
 import Button from '../components/buttons/Button.vue';
 import { useRoute } from 'vue-router';
 
--- src/views/vault_project.vue
+++ src/views/vault_project.vue
@@ -5,30 +5,23 @@ import banner from './Banner.vue';
 <template>
   <div>
     <banner />
-      <div class="flex flex-col items-center mt-20">
-      </div> 
+    <div class="flex flex-col items-center mt-20">
+    </div> 
   </div>
-    <div class="container mx-auto p-4">
-      <h1 class="text-3xl font-bold mb-4">Role: Data Scientist</h1>
-      <h2 class="text-2xl font-semibold mb-2">Technologies Used:</h2>
-      <ul class="list-disc list-inside mb-4">
-        <li>R</li>
-        <li>SQL</li>
-      </ul>
-      <p class="text-lg mb-4">
-        During my time as a data scientist with Vaulted, I was given the opportunity to help create and validate the accuracy of machine learning models. The models that we built were used for creating expected player and team statistics. Along with aiding in the building of these machine learning models using XGBoost, I was able to test their accuracy and reliability using statistical analysis in R for both MLB (American Professional) and NPB (Japanese Professional) data. Below you will see the testing of the swing decision and the ball in play outcome models. Also, attached are SQL querys I was tasked with creating to identify different key player statistics.
-      </p>
-      <div class="border border-gray-300 p-6 rounded-lg w-full max-w-4xl mx-auto">
-        <h3 class="text-xl font-semibold mb-2">Code:</h3>
-        <pre class="text-sm bg-gray-100 p-4 rounded-lg text-blue-500 overflow-x-auto">
-Link or File to Github Code w/ Dummy Data
-        </pre>
-      </div>
-      <div class="border border-gray-300 p-6 rounded-lg w-full max-w-4xl mx-auto">
-        <h3 class="text-xl font-semibold mb-2">SQL File/ or same thing:</h3>
-        <pre class="text-sm bg-gray-100 p-4 rounded-lg text-blue-500 overflow-x-auto">
-Code #2   
-  </pre>
-      </div>
-    </div>
-  </template>
+  <div class="container mx-auto p-4">
+    <h1 class="text-3xl font-bold mb-4">Role: Data Scientist</h1>
+    <h2 class="text-2xl font-semibold mb-2">Technologies Used:</h2>
+    <ul class="list-disc list-inside mb-4">
+      <li>R</li>
+      <li>SQL</li>
+    </ul>
+    <p class="text-lg mb-4">
+      During my time as a data scientist with Vaulted, I was given the opportunity to help create and validate the accuracy of machine learning models. The models we built were used to create expected player and team statistics. Along with aiding in the building of these machine learning models using XGBoost, I was able to test their accuracy and reliability using statistical analysis in R for both MLB (American Professional) and NPB (Japanese Professional) data. Below is a link to the code which tests the validity of the ball in play outcome model. Also, attached are SQL queries I was tasked with creating to identify different key player statistics.
+    </p>
+    <a href="https://github.com/breinke27/brian-vue-portfolio/blob/main/Vaulted Baseball" 
+   target="_blank" 
+   class="text-blue-500 py-2 px-4 rounded border border-transparent hover:border-red-500 hover:text-red-500">
+   View Code on GitHub
+</a>
+  </div>
+</template>
