caa82131ab868e9482778e3ebe19ab14ff9c56ab
breinke27 <brian.reinke@richmond.edu>
2024-08-17 10:21:41 -0400

    new button constuction and layout

--- src/views/Banner.vue
+++ src/views/Banner.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import Button from '../components/buttons/Button.vue';
 import { useRoute } from 'vue-router';
 
@@ -20,6 +20,33 @@ const route = useRoute();
       <template v-else-if="route.path !== '/more_about_projects'">
         <Button title="Read More About My Projects" link="/more_about_projects" class="font-bold mr-4 rounded-lg"/>
       </template>
+      <Button title="LinkedIn" link="https://www.linkedin.com/in/brianreinke21" target="_blank" class="font-bold mr-4 rounded-lg" />
+    </div>
+  </div>
+</template> -->
+<script setup>
+import Button from '../components/buttons/Button.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+</script>
+
+<template>
+  <div class="bg-gray-800 text-white py-4 w-full fixed top-0 left-0 flex justify-between items-center">
+    <span class="font-bold text-xl ml-4 rounded-lg px-2 py-3">
+      Brian Reinke's Portfolio
+    </span>
+    <div class="flex items-center">
+      <template v-if="route.path === '/database_project' || route.path === '/seedmetrics_project' || route.path === '/vaulted_project'">
+        <Button title="Back to Projects" link="/more_about_projects" class="font-bold mr-4 rounded-lg" />
+      </template>
+      <template v-if="route.path !== '/'">
+        <Button title="Home" link="/" class="font-bold class= mr-4 rounded-lg" />
+      </template>
+      <template v-else-if="route.path !== '/more_about_projects'">
+        <Button title="Read More About My Projects" link="/more_about_projects" class="font-bold mr-4 rounded-lg"/>
+      </template>
+      <Button title="Resume" :link="'/Brian Reinke Resume.pdf'" target="_blank" class="font-bold mr-4 rounded-lg" />
       <Button title="LinkedIn" link="https://www.linkedin.com/in/brianreinke21" target="_blank" class="font-bold mr-4 rounded-lg" />
     </div>
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <script setup>
-import Button from '../components/buttons/Button.vue';
 import banner from './Banner.vue';
 import { useRoute } from 'vue-router';
 import headshot from '/headshot.png';
@@ -13,15 +12,12 @@ const route = useRoute();
     <div class="mt-16 text-center border-black border-4 rounded-lg p-8 bg-white">
       <div class="flex flex-col md:flex-row items-center justify-center">
         <img :src="headshot" alt="Brian Reinke" class="w-64 h-auto shadow-lg mb-6 md:mb-0 md:mr-8 rounded-lg" />
-        <p class="text-lg leading-relaxed max-w-lg">
-          Hello and welcome to my portfolio! Here you will find a collection of projects I have worked on in the past year as well as my resume. I'm a rising senior at the University of Richmond and am passionate about data engineering, data science, and web development. Thank you for taking the time to explore!
-        </p>
-      </div>
-      <Button title="Resume" :link="'/Brian Reinke Resume.pdf'" target="_blank" class="mt-8" />
-    </div>
-    <div> 
-      <div class="mt-16 text-center border-black border-4 rounded-lg p-8 bg-white">
-        <h3 class="font-bold">This Project was built using Vue.js and JavaScript</h3>
+        <div class="flex flex-col">
+          <p class="text-lg leading-relaxed max-w-lg">
+            Hello and welcome to my portfolio! Here you will find a collection of projects I have worked on in the past year as well as my resume. I'm a rising senior at the University of Richmond and am passionate about data engineering, data science, and web development. Thank you for taking the time to explore!
+          </p>
+          <p class="italic mt-4">This Project was built using Vue.js and JavaScript</p>
+        </div>
       </div>
     </div>
   </div>
